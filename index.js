e667559fea2bc20f8a2c11f03081b21eeec7f514
Enoc Martinez <Enoc.Martinez@ibm.com>
2019-09-18 12:49:41 -0500

    cors added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
